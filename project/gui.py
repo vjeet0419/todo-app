c9461f888eb5e9b9d5d036c34dcdcad3f9f8713e
jeet <vishwajeetsingh41999@gmail.com>
2023-01-09 03:15:30 +0530

    app name change

--- project/gui.py
+++ project/gui.py
@@ -4,6 +4,6 @@ add_text=sg.Text("enter a todo")
 input_text=sg.InputText(tooltip="enter here")
 add_button=sg.Button("ADD")
 
-window=sg.Window("todo app" , layout=[[add_text,input_text,add_button]])
+window=sg.Window("todo application" , layout=[[add_text,input_text,add_button]])
 window.read()
 window.close()
