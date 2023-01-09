c909a79f321c0fa9c831f53dcab0eba41ebc9e8d
jeet <vishwajeetsingh41999@gmail.com>
2023-01-09 22:00:07 +0530

    changes in the get todos function in cli

--- project/cli.py
+++ project/cli.py
@@ -19,7 +19,7 @@ while True:
 
         todos= functions.get_todos()
 
-        todos.append(todo + "\n")
+        todos.append(todo )
 
         functions.write_todos(todos)
 
--- project/gui.py
+++ project/gui.py
@@ -4,8 +4,12 @@ import PySimpleGUI as sg
 add_text=sg.Text("Enter a todo")
 input_text=sg.InputText(tooltip="enter here", key="todo")
 add_button=sg.Button("ADD")
+edit_button=sg.Button("EDIT")
+list_box=sg.Listbox(values=[functions.get_todos()],enable_events=True,
+                    size=[45, 10])
 
-window=sg.Window("todo application" , layout=[[add_text,input_text,add_button]],font=('helvetica', 20))
+window=sg.Window("todo application" , layout=[[add_text,input_text,add_button],[list_box, edit_button]],
+                 font=('helvetica', 20))
 while True:
     event,value=window.read()
     print(event)
@@ -13,7 +17,7 @@ while True:
     match event:
         case "ADD":
             todos=functions.get_todos()
-            new_todos=value["todo"]
+            new_todos=value["todo"]+ '/n'
             todos.append(new_todos)
             functions.write_todos(todos)
         case sg.WINDOW_CLOSED:
